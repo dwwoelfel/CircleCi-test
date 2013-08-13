ac5e6b61ff6ed222b8bb38ebad967340e5a44492
Housni <housni.yakoob@gmail.com>
2013-08-13 23:10:25 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	$dsn = 'mysql:host=localhost;port=3306;dbname=circle_test';
+	$login = 'ubuntu';
+	$password = '';
+	$options = [
+		PDO::ATTR_PERSISTENT => true,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+	];
+
+	try {
+		$connection = new PDO($dsn, $login, $password, $options);
+		echo '<pre>' . print_r($connection, true) . '</pre>';
+	} catch (PDOException $e) {
+		throw new Exception($e->getMessage(), $e->getCode());
+	}
+?>
